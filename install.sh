dc82b8a63ed6e262dcd03b98b095941a88ed7be0
127.0.0.1 <mandarrapi@gmail.com>
2026-02-11 16:52:46 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,18 @@
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+clear
+echo -e "${GREEN}Installing Proxy...${ENDCOLOR}"
+sleep 1
+if [ -f "shift" ]; then
+    echo -e "${RED}Deleting old proxy...${ENDCOLOR}"
+    rm ascent
+    sleep 1
+    echo -e "${GREEN}Getting proxy...${ENDCOLOR}"
+fi
+wget -q https://github.com/NicholasDevelopers/proxy/raw/main/shift
+sleep 1
+echo -e "${GREEN}Proxy Installed${ENDCOLOR}"
+echo -e "${GREEN}Execute proxy with this command: ./shift${ENDCOLOR}"
+chmod +x shift
